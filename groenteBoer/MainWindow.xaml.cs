f98149cc204b8ab48a9186b7d56223175d74150f
IDontCaramel <lucaskoot@icloud.com>
2024-06-14 12:05:05 +0200

    reads/displays data from datbase

--- groenteBoer/MainWindow.xaml.cs
+++ groenteBoer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,90 @@ namespace groenteBoer
         public MainWindow()
         {
             InitializeComponent();
+            generateGroente();
+        }
+
+        private void generateGroente()
+        {
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            List<Product> products = dbHelper.GetProducts();
+
+            foreach (Product product in products)
+            {
+                Button btn = new Button
+                {
+                    Tag = product.ID,
+                    Margin = new Thickness(4),
+                    Height = 250,
+                    Width = 250,
+
+                };
+                if (product.plaatje != null)
+                {
+                    Uri uri = new Uri("pack://application:,,,/fruits/" + product.plaatje);
+
+                    Grid grid = new Grid();
+
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Text = $"{product.groente}\n{product.prijs.ToString()} {product.prijsPer}";
+                    textBlock.Foreground = Brushes.Black;
+                    textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                    textBlock.VerticalAlignment = VerticalAlignment.Center;
+                    textBlock.TextAlignment = TextAlignment.Center;
+                    textBlock.FontSize = 30;
+                    textBlock.FontFamily = new FontFamily("Century Gothic");
+
+
+
+                    try
+                    {
+                        Image imageControl = new Image();
+                        BitmapImage bitmapImage = new BitmapImage(uri);
+                        imageControl.Source = bitmapImage;
+                        imageControl.VerticalAlignment = VerticalAlignment.Center;
+                        imageControl.HorizontalAlignment = HorizontalAlignment.Center;
+
+                        textBlock.VerticalAlignment = VerticalAlignment.Bottom;
+                        
+                        grid.Children.Add(imageControl);
+                    }
+                    catch
+                    {
+                    }
+
+                    grid.Children.Add(textBlock);
+
+                    btn.Content = grid;
+                }
+
+
+                else
+                {
+                    StackPanel stackPanel = new StackPanel();
+
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Text = product.prijs.ToString(); // Assuming product.prijs is a numeric value
+                    textBlock.Foreground = Brushes.Black; // Set text color
+                    textBlock.HorizontalAlignment = HorizontalAlignment.Center; // Center text horizontally
+                    textBlock.VerticalAlignment = VerticalAlignment.Center; // Center text vertically
+
+                    stackPanel.Children.Add(textBlock);
+                    btn.Content = stackPanel;
+                }
+
+                // Subscribe to the Click event of the button
+                btn.Click += (sender, args) =>
+                {
+                    Button clickedButton = sender as Button;
+                    if (clickedButton != null)
+                    {
+                        MessageBox.Show("Button clicked! Tag: " + clickedButton.Tag.ToString());
+                    }
+                };
+
+                // Add the button to your layout container
+                wp.Children.Add(btn);
+            };
         }
     }
 }
---
+++ groenteBoer/getDatabase.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace groenteBoer
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string groente { get; set; }
+        public decimal prijs { get; set; }
+        public string plaatje { get; set; }
+        public string soort { get; set; }
+        public string prijsPer { get; set; }
+    }
+
+    public class DatabaseHelper
+    {
+        private string connectionString;
+
+        public DatabaseHelper()
+        {
+            connectionString = "Server=localhost;Database=groenteboer;user=root;";
+        }
+
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT id, groente, prijs, plaatje, soort, prijsPer FROM producten";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                try
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        
+
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                ID = reader.GetInt32("id"),
+                                groente = reader.GetString("groente"),
+                                prijs = reader.GetDecimal("prijs"),
+                                plaatje = reader.GetString("plaatje"),
+                                soort = reader.GetString("soort"),
+                                prijsPer = reader.GetString("prijsPer")
+                            };
+
+                            products.Add(product);
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"General error: {ex.Message}");
+                }
+            }
+
+            return products;
+        }
+
+    }
+}
